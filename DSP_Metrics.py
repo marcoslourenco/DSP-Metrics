b682e4b1fee1b4d8e2d1fb69cc3f8aaf0d1d800a
marcoslourenco <40247083+marcoslourenco@users.noreply.github.com>
2018-12-11 08:39:38 +0000

    Add files via upload

--- DSP_Metrics.py
+++ DSP_Metrics.py
@@ -5,44 +5,78 @@ Created on Sun Nov 25 14:48:03 2018
 @author: MLOURE13
 """
 import pandas as pd
-import numpy as np
+import time as t
 
-class DataAggregation:
-    def __init__(self, dfWeeks, dfApps, dfCS, dfDays, dfCompleteSet ):
-        self.dfWeeks=dfWeeks
-        self.dfApps=dfApps
-        self.dfCS=dfCS
-        self.dfDays=dfDays
-        self.dfCompleteSet=dfCompleteSet        
-        example=['haloooo']
-        example1=['tudo bem?']
-        example3=example+example1
-        print(example3)
-    
-    def hj(self):
-        self.dfWeeks=1
-        return self.dfWeeks
+start_time=t.time()
+
+
+class App_Succ_Rate(object):
+    def __init__(self,app_id, cur_state, req_date, req_time):
+        self.app_id=app_id
+        self.cur_state=cur_state
+        self.req_date=req_date
+        self.req_time=req_time
+        self.app_data={}
+        self.app_data[str(self.cur_state)]=self.req_date             
         
+    def __repr__(self):
+        return repr([self.app_id, self.cur_state, self.req_date, self.req_time, self.app_data]) 
     
+      
 df=pd.read_table('class.txt', sep=',', header=0)
 df['WK']=df['DATEREQUESTED'].map(lambda x : x[0:9]).map(lambda x: pd.datetime.strptime(x,'%d-%b-%y')).map(lambda x: pd.datetime.isocalendar(x)[1])
 df['Date']=df['DATEREQUESTED'].map(lambda x : x[0:9]).map(lambda x: pd.datetime.strptime(x,'%d-%b-%y'))
 df['Time']=df['DATEREQUESTED'].map(lambda x: x[9:18]).map(lambda x: x.replace('.',':')).map(lambda x: x.strip())
 df.drop('DATEREQUESTED', axis=1, inplace=True)
+
+''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+'''''''''''''''''''''''''''''''''''''''''' # Unique Lists'''''''''''''''''''''''''''''''''''''''''
+''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
 Unique_APP=df['APPID'].drop_duplicates(keep='first')
 Unique_CS=df['CURRENTSTATE'].drop_duplicates(keep='first')
 Unique_Days=df['Date'].drop_duplicates(keep='first')
 Unique_Weeks=df['WK'].drop_duplicates(keep='first')
+dur=(t.time()-start_time)
 
-#print(example)
+#h=App_Succ_Rate('G1265','REQ_START','10 DEC 2018','10:24:49')
 
-gf=DataAggregation(Unique_Weeks, Unique_APP, Unique_CS, Unique_Days, df)
-print(gf.hj())
+def success_rate(Unique_APP, Unique_CS, df):
+    start_time=t.time()
+    app_count=0
+    error_count=0
+    suc_count=0
+    start_count=0
+    abort_count=0
+    
+    error_state=['ERROR', 'DOWNLOAD_APPLICATION_FAILURE', 'COMMS_ERROR']
+    succe_state=['FINISHED', 'DOWNLOAD_APPLICATION_SUCCESS','INDICTED']
+    start_state=['REQUESTED_START','REQUESTED_DOWNLOAD']
+    abort_state=['ABORTED']
+    
+    for app in Unique_APP:
+        for i in range(len(df)):
+            if df['APPID'][i] == app:
+                if str(df['CURRENTSTATE'][i]) in error_state:
+                    error_count+= 1
+                elif str(df['CURRENTSTATE'][i]) in succe_state:
+                    suc_count+=1
+                elif str(df['CURRENTSTATE'][i]) in start_state:
+                    start_count+=1
+                elif str(df['CURRENTSTATE'][i]) in abort_state:
+                    abort_count+=1
+                    
+                app_count=app_count+1
+                #print(app, count, df['APPID'][i])
+        print(app, app_count, error_count, suc_count, start_count, abort_count)
+        app_count=0
+        error_count=0
+        suc_count=0
+        start_count=0
+        abort_count=0
+        dur=(t.time()-start_time)
+        print('It took : ' + str(dur))                              
+sr=success_rate(Unique_APP, Unique_CS, df)
 
-#for i in range(len(Unique_Weeks)):
-#Unique_Weeks.to_csv('sian_unique_weeks.txt', sep='|', index=False)
-#np.savetxt('sian_unique_weeks.txt', Unique_CS, delimiter='|')
-        
-    #print(Unique_Weeks[i])
-    #print(df['WK'][i])
-    #print(df['APPID'][i])
+#print(h)
+print('It took: ' + str(dur) + ' seconds. ')
